2a001b74cce868483b0a055ea4dc905e6afe264c
Adwaithck <ac23aeh@herts.ac.uk>
2024-11-29 14:47:03 +0000

    scatter plot visualization.R file

---
+++ visualization.R
@@ -0,0 +1,18 @@
+df <- read.csv("25.csv")  # Reading the CSV file and storing the dataset in the df variable
+
+x <- df$calories_burned # Independent variable
+y <- df$step_count # Dependent variable
+
+#scatter plot
+plot(
+  x, y,
+  main = "Scatter plot showing the correlation between 
+          step count and calories burned",
+  xlab = "Calories burned (Cal)",
+  ylab = "Step count (in thousands)",
+  pch = 19,
+  frame = TRUE
+)
+
+model <- lm(y ~ x, data = df) # Fitting a linear model to the cleaned data
+abline(model, col = "blue")         # Adding the regression line
