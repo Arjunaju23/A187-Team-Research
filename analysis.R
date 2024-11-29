830496632bb6f07f9090ae61d52492f82b2890d0
AyishaSherinPilakkal <pa23acc@herts.ac.uk>
2024-11-29 14:58:39 +0000

    analysis.R file

--- analysis.R
+++ analysis.R
@@ -4,5 +4,4 @@ x <- df$step_count # Dependent variable
 y <- df$calories_burned# Independent variable
 
 # Perform Spearman and Kendall correlation tests
-cor.test(y, x, method = "spearman")
-cor.test(y, x, method = "kendall")
+cor.test(x, y, method = "spearman")
